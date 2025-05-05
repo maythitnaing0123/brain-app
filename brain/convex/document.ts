74d8b5ff4421527aaac5ab8e54167030e174dd3b
maythitnaing0123 <maythitnaing44@gmail.com>
2025-05-05 16:28:33 +0630

    "initial db"

--- brain/convex/document.ts
+++ brain/convex/document.ts
@@ -1,18 +1,24 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
-
 
 // one table - accept all fun.
 //get,update,delete.
 
 export const getDocument = query({
-    args: {},
-    async handler(ctx){
-        return await ctx.db.query("documents").collect()
+  args: {},
+  async handler(ctx) {
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+
+    if (!userId) {
+      return [];
     }
-})
 
-
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
+      .collect();
+  },
+});
 
 //create function to import db. (server side fun)
 export const createDocument = mutation({
@@ -20,8 +26,15 @@ export const createDocument = mutation({
     title: v.string(),
   },
   handler: async (ctx, args) => {
+    const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+
+    if (!userId) {
+      throw new ConvexError("Not Authenticated");
+    }
+
     await ctx.db.insert("documents", {
       title: args.title,
+      tokenIdentifier: userId,
     });
   },
 });
